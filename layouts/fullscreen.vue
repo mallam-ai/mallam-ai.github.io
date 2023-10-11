80a88455773b173dcd2dfaffade3c9e3c6ddff35
GUO YANKE <hi@guoyk.xyz>
2023-10-11 23:16:34 +0800

    feat: add page /dataset

---
+++ layouts/fullscreen.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center"
+  >
+    <slot></slot>
+  </div>
+
+  <div class="absolute bottom-0 left-0 w-full flex flex-col items-center pb-8">
+    <Footer></Footer>
+  </div>
+</template>
