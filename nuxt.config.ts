fd2e442fff9ce96c757a0af40023b5585bff2a9e
GUO YANKE <hi@guoyk.xyz>
2023-11-01 16:02:53 +0800

    debug: check secret expose

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
-  ui: {
-    icons: ["heroicons", "simple-icons", "mdi", "noto-v1"],
-  },
+    devtools: {enabled: true},
+    modules: ["@nuxt/ui"],
+    ui: {
+        icons: ["heroicons", "simple-icons", "mdi", "noto-v1"],
+    },
+    runtimeConfig: {
+        backend: {
+            secretKey: process.env.BACKEND_SECRET_KEY,
+        }
+    }
 });
---
+++ pages/debug.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  titleLabel: "Debug",
+  titleIcon: "i-mdi-toolbox",
+});
+
+const config = useRuntimeConfig()
+const backendSecretKeySize = config.backend.secretKey.length
+</script>
+<template>
+  <span>{{backendSecretKeySize}}</span>
+</template>
